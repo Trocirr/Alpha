c252730b97938c8cdc6d3fe80b1fe24e7800497c
Trocirr <38221031+Trocirr@users.noreply.github.com>
2018-08-18 23:58:28 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -241,7 +241,7 @@ async def on_message(message):
 
 	if message.content.startswith('-help ping'):
 		embed = discord.Embed(title="__Ping__",description='Usage: `-ping`', color= 0xFF8C00)
-		embed.add_field(name="", value="Check the ping of the bot")
+		embed.add_field(name="Check the ", value="ping of the bot")
 		await client.send_message(message.channel, embed=embed)
 
 
