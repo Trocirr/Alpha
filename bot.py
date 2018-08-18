25cc5e8a1bbd4d77728e62dccfd7433537149567
Trocirr <38221031+Trocirr@users.noreply.github.com>
2018-08-18 23:39:16 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -158,16 +158,22 @@ async def on_message(message):
 		embed.add_field(name="-about", value="Displays the bot's info.", inline=False)
 		embed.set_thumbnail(url='http://pngimg.com/uploads/question_mark/question_mark_PNG130.png')
 		await client.send_message(message.channel, embed=embed)
-
-	if message.content.startswith('-help ping'):
-		embed = discord.Embed(title='__Ping Command__',description='', color=0xFF8C00)
-		embed.add_field(name="Checks the ping of the bot.", value="", inline=False)
+		
+		
+	if message.content.startswith('-help ping') and message.content[5:] =="": 
+		embed = discord.Embed(title="__Ping__",description='', color=0xFF8C00)
 		embed.add_field(name="Usage:", value="`-ping`", inline=False)
+		embed.add_field(name="-help general", value="Shows the general commands.", inline=False)
+		embed.add_field(name="-help games", value="Shows all commands related to the games.", inline=False)
+		embed.add_field(name="-help math", value="Shows all commands related to math.", inline=False)
+		embed.add_field(name="-help fun", value="Shows the commands for fun.", inline=False)
 		embed.set_thumbnail(url='http://pngimg.com/uploads/question_mark/question_mark_PNG130.png')
 		await client.send_message(message.channel, embed=embed)
 
 
-	
+
+
+
 	if message.content.startswith('-help') and message.content[5:] =="": 
 		embed = discord.Embed(title=":scroll:__Alpha® Help__",description='', color=0xFF8C00)
 		embed.add_field(name="-help all", value="Shows the help message for all the categories.", inline=False)
