db6416849466bea0e76a7707f114edd5a2a3f396
Trocirr <38221031+Trocirr@users.noreply.github.com>
2019-01-27 16:41:41 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,6 +12,9 @@ import requests
 Client = discord.Client()
 client = commands.Bot(command_prefix = "-")
 
+chat_filter = ["FUCK", "STFU", "NIGGA", "RETARD", "NOOB", "MORON", "SHIT", "CUNT"]
+bypass_list = []
+
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -19,7 +22,17 @@ async def on_ready():
     print(client.user.id)
     print('------')	
 		
-
+@client.event
+async def on_message(message):
+	contents = message.content.split(" ")
+	for word in contents:
+	    if word.upper() in chat_filter:
+		if not message.author.id in bypass_list:
+		    try:
+			await client.delete_message(message)
+			await client.send_message(message.channel, "Hey! You're not allowed to use that word here!")
+		    espect discord.errors.NotFound:
+			return
 		
 @client.event
 async def on_message(message):
