48b07afceafd0971cc9891311ed96162cfc94ba4
Trocirr <38221031+Trocirr@users.noreply.github.com>
2019-01-27 17:24:58 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
 	if message.content.upper().startswith('NIGGA'):
-		await client.say("Hey! You're not allowed to use that word here"!)
+			await client.send_message(message.channel, "Hey! You're not allowed to use that word here!")
 		
 @client.event
 async def on_message(message):
