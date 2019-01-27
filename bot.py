4767effaa59935c4617f943280b866edf11b4971
Trocirr <38221031+Trocirr@users.noreply.github.com>
2019-01-27 16:59:33 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ import requests
 Client = discord.Client()
 client = commands.Bot(command_prefix = "-")
 
-chat_filter = ["FUCK", "STFU", "NIGGA", "RETARD", "NOOB", "MORON", "SHIT", "CUNT"]
+chat_filter = ["PINEAPPLE", "APPLE", "CHROME"]
 bypass_list = []
 
 @client.event
@@ -24,13 +24,15 @@ async def on_ready():
 		
 @client.event
 async def on_message(message):
-	contents = message.content.split(" ")
-	for word in contents:
-	    if word.upper() in chat_filter:
-		if not message.author.id in bypass_list:
-		    try:
-			await client.delete_message(message)
-			await client.send_message(message.channel, "Hey! You're not allowed to use that word here!")
+    contents = message.content.split(" ") #contents is a list type
+    for word in contents:
+        if word.upper() in chat_filter:
+            if not message.author.id in bypass_list:
+                try:
+                    await client.delete_message(message)
+                    await client.send_message(message.channel, "**Hey!** You're not allowed to use that word here!")
+                except discord.errors.NotFound:
+                    return
 		
 @client.event
 async def on_message(message):
