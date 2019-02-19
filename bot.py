bd1d7ad84c927d8e2b66d4a8b7bc70b1e10087ff
Trocirr <38221031+Trocirr@users.noreply.github.com>
2019-02-19 21:15:39 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -219,7 +219,7 @@ async def on_message(message):
 			embed = discord.Embed(title='', color=0xFFFF00)
 			embed.add_field(name="__Suggestion__", value= "```%s```" % (" ".join(args[1:])), inline=False)
 			embed.add_field(name="By:", value="**{0.author.mention}**".format(message), inline=False)
-			embed.set_thumbnail(url='https://openclipart.org/image/2400px/svg_to_png/10515/yves-guillou-idea.png')
+			embed.set_thumbnail(url = user.avatar_url)
 			a=await client.send_message(channel, embed=embed)
 			await client.add_reaction(a, "👍")
 			await client.add_reaction(a, "👎")
